abeaf2a3d5f24776d7dde0c7910c80d198c7893e
qarosedias <qarosedias@gmail.com>
2022-10-19 19:26:43 -0300

    update tests

--- cypress/e2e/features/Home.feature
+++ cypress/e2e/features/Home.feature
@@ -4,9 +4,12 @@ Feature: Home Page Site Zitrus
 Home Page Functionality.
 
 Background:
- Scenario: Validate from the Main Page Menus  
-  Given a web browser is on the Zitrus homepage
-  When I view the main page menus
-  Then I should see the following menus About Us, Solutions, Blog and Careers
+ Scenario: Validate from Home Page 
+  Given that a web browser is on the About Us page
+  When I view the main page menu
+  Then I should see the following menus About Us
 
  Scenario: Validate Main Page Address 
+  Given that a web browser is on the homepage
+  When I see "R. Xavantes, 54 - 2nd floor. - Atiradores, Joinville - SC, 89203-210 "
+  Then confirm the address
--- cypress/e2e/step_definitions/Home.js
+++ cypress/e2e/step_definitions/Home.js
@@ -2,14 +2,26 @@ import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 const Home = require('../../pages/Home');
 
 describe('Home Page Zitrus', () => {
-    context('Menus', () => {
-        Given('a web browser is on the Zitrus homepage', () => {
-            cy.visit('/');
+    beforeEach(() => {
+        cy.visit('/')
+    })
 
-            When('I view the main page menus', () => {      
-  
-                Then('I should see the following menus About Us, Solutions, Blog and Careers', () => {
-          
+    context('Menus', () => {
+        Given('that a web browser is on the About Us page', () => {
+            cy.get('.elementor-container:nth-child(2) .elementor-element-6bf5647 .elementor-widget-wrap').click();
+            cy.url().should('contains', 'https://zitrus.com.br/quem-somos/');
+            .should('exist')
+
+            When('I see "Commitment to Result and Transformation"', () => {      
+                cy.get('.elementor-heading-title elementor-size-default')
+                .find('h2')
+                .should('have.text', 'Commitment to Result and Transformation')
+              
+                Then('I should see the following menus About Us', () => {
+                    cy.get('elementor-heading-title elementor-size-default')
+                    .should('exist')
+                    .and('have.class', 'x-large')
+                                                       
                 });        
             });
         });
@@ -18,13 +30,19 @@ describe('Home Page Zitrus', () => {
         Given('', () => {
             cy.visit('/');
             When('', () => { 
+                cy.get('.elementor-icon-list-text')
+                .find('h2')
+                .should('have.text', 'R. Xavantes, 54 - 2nd floor. - Atiradores, Joinville - SC, 89203-210 ')
 
-                Then('', () => {      
+                Then('', () => {
+                    cy.get('.elementor-icon-list-text')
+                    .should('have.text', 'R. Xavantes, 54 - 2nd floor. - Atiradores, Joinville - SC, 89203-210 ')  
+                    .should('exist')    
                 });        
             });
         });
     }); 
-    context('Plus', () => {
+    context('Proximo Test', () => {
         Given('', () => {
             cy.visit('/');
             When('', () => {
